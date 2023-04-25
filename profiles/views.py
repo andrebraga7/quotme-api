5d7127fddf08dd276a2709d6aebcf9280ed77a5b
andrebraga7 <andrestrevisan@gmail.com>
2023-04-25 17:58:57 +0000

    Add the ProfileList and ProfileDetail views

--- profiles/views.py
+++ profiles/views.py
@@ -7,3 +7,9 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
