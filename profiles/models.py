33d52c0405c22d6f4c3cd3701b2e567a4acaca33
andrebraga7 <andrestrevisan@gmail.com>
2023-04-25 17:40:35 +0000

    Add a post_save receiver to the profile model, to auto create an instance from the User

--- profiles/models.py
+++ profiles/models.py
@@ -20,3 +20,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
