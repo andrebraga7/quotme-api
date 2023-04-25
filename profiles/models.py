76be2fc65d41cbd28ad0cb6062e76369fa308ef2
andrebraga7 <andrestrevisan@gmail.com>
2023-04-25 17:31:45 +0000

    Create the profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_gozpfa'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
--- quotme_api/urls.py
+++ quotme_api/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
+    path('', include('profiles.urls')),
 ]
