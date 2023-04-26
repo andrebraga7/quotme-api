d5af8cfd6ace9799eb80e26aa907614592b984ac
andrebraga7 <andrestrevisan@gmail.com>
2023-04-26 13:35:46 +0000

    Create the SaveSerializer

---
+++ saved/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Save
+
+
+class SaveSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Save
+        fields = [
+            'id', 'owner', 'quote', 'created_at '
+        ]
