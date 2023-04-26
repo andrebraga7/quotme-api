6ca6e50ff507e29b27ac978fd821b7dba63168b5
andrebraga7 <andrestrevisan@gmail.com>
2023-04-26 14:03:45 +0000

    Create the CommentSerializer

---
+++ comments/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'quote', 'created_at', 'updated_at',
+            'content'
+        ]
