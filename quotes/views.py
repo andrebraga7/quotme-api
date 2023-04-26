714e851b0d6b89518505e24c8dd3334d54694fce
andrebraga7 <andrestrevisan@gmail.com>
2023-04-26 12:15:28 +0000

    Add a perform_update to the QuoteDetail view to assign an author to it This will take the serialize validated_data and get or create a new author instance. It will then update the data with the retrieved author instance

--- quotes/views.py
+++ quotes/views.py
@@ -12,7 +12,6 @@ class QuoteList(generics.ListCreateAPIView):
     queryset = Quote.objects.all()
 
     def perform_create(self, serializer):
-
         # Retrieve the author name from the serializer validated_data
         # Get or create a new author instance with the given name
         # Save method to create a new quote instance
@@ -26,3 +25,9 @@ class QuoteDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = QuoteSerializer
     queryset = Quote.objects.all()
+
+    def perform_update(self, serializer):
+        # Same logic as the perform_create but this will run when updating
+        name = serializer.validated_data['author']
+        author_instance, created = Author.objects.get_or_create(name=name)
+        serializer.save(author=author_instance)
