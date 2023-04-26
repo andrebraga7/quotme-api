c7cd87d5db894b791ecefa5e296b92c5076c6ce8
andrebraga7 <andrestrevisan@gmail.com>
2023-04-26 18:00:37 +0000

    Add extra fields to the profile serializer fields added are: quotes_count, followers_count and following_count

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.db.models import Count
 from quotme_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -6,7 +7,11 @@ from .serializers import ProfileSerializer
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        quotes_count=Count('owner__quote', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
