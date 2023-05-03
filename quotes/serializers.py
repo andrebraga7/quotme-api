67fa62523e84f2db30847c9d4447c09044b0d541
andrebraga7 <andrestrevisan@gmail.com>
2023-05-03 12:08:35 +0000

    Add save_id and author_id to the quote serializer

--- quotes/serializers.py
+++ quotes/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Quote
 from likes.models import Like
+from saved.models import Save
 
 
 class QuoteSerializer(serializers.ModelSerializer):
@@ -11,6 +12,8 @@ class QuoteSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    save_id = serializers.SerializerMethodField()
+    author_id = serializers.ReadOnlyField(source='author.id')
     comments_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
 
@@ -27,10 +30,19 @@ class QuoteSerializer(serializers.ModelSerializer):
             return like.id if like else None
         return None
 
+    def get_save_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            save = Save.objects.filter(
+                owner=user, quote=obj
+            ).first()
+            return save.id if save else None
+        return None
+
     class Meta:
         model = Quote
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'category',
             'author', 'content', 'is_owner', 'profile_id', 'profile_image',
-            'like_id', 'comments_count', 'likes_count'
+            'like_id', 'save_id', 'author_id', 'comments_count', 'likes_count'
         ]
