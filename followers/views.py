b819f84e8afdef71d6bda0935525b795c5431d3e
andrebraga7 <andrestrevisan@gmail.com>
2023-05-04 18:44:26 +0000

    Add a perform create function to the followers list view

--- followers/views.py
+++ followers/views.py
@@ -10,6 +10,9 @@ class FollowerList(generics.ListCreateAPIView):
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class FollowerDetail(generics.RetrieveDestroyAPIView):
 
